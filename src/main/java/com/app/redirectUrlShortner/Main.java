6a4c11f75e3f666c764c347bf9a7b9eb39d2de9d
karinaerikads <karinaerikads48@gmail.com>
2024-11-20 20:25:18 -0300

    feat: adiciona método que faz redirecionamento da url

--- src/main/java/com/app/redirectUrlShortner/Main.java
+++ src/main/java/com/app/redirectUrlShortner/Main.java
@@ -1,7 +1,63 @@
 package com.app.redirectUrlShortner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String pathParameters = (String) input.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
+
+        if (shortUrlCode == null || shortUrlCode.isEmpty()){
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required");
+        }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("url-shortener-storage-database-db")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception e){
+            throw new RuntimeException("Error fetching URL data from S3: " + e.getMessage(), e);
+        }
+
+        UrlData urlData;
+        try {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception exception){
+            throw new RuntimeException("Error deserializing URL data: " + exception.getMessage(), exception);
+        }
+
+        long currentTimeInSecond = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (urlData.getExpirationTime() < currentTimeInSecond ){
+
+            response.put("statusCode", 410);
+            response.put("body", "URL has expirede");
+            return response;
+        }
+
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        return response;
     }
 }
