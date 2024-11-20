f48daf7900cf5f86cff5f86aa1f55874ae34c9c6
karinaerikads <karinaerikads48@gmail.com>
2024-11-20 20:24:41 -0300

    feat: adiciona dto com dados da url

--- src/main/java/com/app/redirectUrlShortner/UrlData.java
+++ src/main/java/com/app/redirectUrlShortner/UrlData.java
@@ -1,10 +1,12 @@
-package com.app.createurlshortner;
+package com.app.redirectUrlShortner;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class UrlData {
